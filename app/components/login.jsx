9385c8df7a8dc4cd1c2374959893dd22d16b30fb
Cecezobam <ifyzobam86@gmail.com>
2024-06-25 16:39:55 +0100

    added if,else .

--- app/components/login.jsx
+++ app/components/login.jsx
@@ -1,19 +1,44 @@
 "use client";
-import { Input, Button, Form, Card } from "antd";
+import { Card, Input, Form, Button, message } from "antd";
+import { useState } from "react";
+import {useRouter} from "next/navigation";
 export default function Login() {
+
+var[ email,setEmail]=useState();
+var[password,setPassword]=useState("");
+var router = useRouter();
+
+  function handleEmail(evt){
+    setEmail(evt.target.value);
+    
+  }
+  function handlePassword(evt){
+  setPassword(evt.target.value);
+    
+  }
+  function handleLogin(){
+   if(email=="cece@yahoo.com"&& password=="12345"){
+      message.success("login sucessful");
+      router.push("/dashboard");
+    }
+    else{
+      message.error("login fail");
+    }
+   
+  }
   return (
     <Card title="My Login" className=" m-auto mt-40 sm:w-1/2">
       <p>Please Enter your Detail</p>
       <Form>
         <Form.Item>
-          <Input placeholder="Enter Email" />
+          <Input placeholder="Enter Email" onChange={handleEmail} />
         </Form.Item>
 
         <Form.Item>
-          <Input.Password placeholder="Enter Password" />
+          <Input.Password placeholder="Enter Password" onChange={handlePassword}/>
         </Form.Item>
       </Form>
-      <Button type="primary">Login</Button>
+      <Button type="primary"onClick={handleLogin}>Login</Button>
     </Card>
   );
 }
---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import { Card, Typography } from "antd";
+
+
+
+export default function Dashboard(){
+    return(
+        <Card title=" Timer Dashboard">
+        <Typography.Title>MYy Dashboard</Typography.Title >
+        </Card>
+    )
+}
